1fa1bfff3afc710a9e359b4eb8f807599eb1f5d5
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-10-12 00:52:26 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        
+        answer = my_string.replace(letter, "");
+        
+        
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
