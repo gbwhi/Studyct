111d92ec5790601f16a421e42548020fe0bcfef8
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-10-13 00:21:42 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,28 +1,17 @@
 class Solution {
     public int solution(int n, String control) {
-       int answer = n;
-	        
-	        String[] arr = control.split("");
-	        
-	        for(int i = 0 ; i<control.length() ; i ++ ){
-	            if(arr[i].equals("w")) {
-	            	answer += 1;
-	            } else if(arr[i].equals("s")) {
-	            	answer -= 1;
-	            } else if(arr[i].equals("d")) {
-	            	answer += 10;
-	            } else if(arr[i].equals("a")) {
-	            	answer -= 10;
-	            }
-	            
-	            
-	            
-	        }
-	        
-	        
-	        
-	        
-	        
-	        return answer;
-	    }
-	}
+        int answer = n;
+
+        for(char ch : control.toCharArray()) {
+            switch(ch) {
+                case 'w': answer += 1; break;
+                case 's': answer -= 1; break;
+                case 'd': answer += 10; break;
+                case 'a': answer -= 10; break;
+                default:break;
+            }
+        }
+
+        return answer;
+    }
+}
