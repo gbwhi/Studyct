4e9580080b517a74ef418aacb8bc335919882f01
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-10-21 17:07:02 +0900

    [level 1] Title: 덧칠하기, Time: 45.39 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        for(int i = 1 ;i<=n ; i++)
+        { for(int j=0 ; j<section.length;j++)
+            if(section[j]==i){
+                i= i+m;
+                answer ++;
+            }
+        }
+        return answer;
+    }
+}
