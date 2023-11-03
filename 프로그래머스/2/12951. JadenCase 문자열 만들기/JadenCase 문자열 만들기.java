e3692e3fc20d7491d3c44c41b6860787dd94e242
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-11-03 15:28:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,32 @@
+class Solution {
+	public String solution(String s) {
+
+		s = s.toLowerCase();
+
+		StringBuilder answer = new StringBuilder();
+
+		String[] arr = s.split("");
+
+		char[] arr1 = s.toCharArray();
+
+		if (Character.isLowerCase(arr1[0])) {
+			arr[0] = arr[0].toUpperCase();
+		}
+		answer = answer.append(arr[0]);
+
+		for (int i = 1; i < s.length()-1; i++) {
+			if (arr[i].equals(" ") ) {
+				if (Character.isLowerCase(arr1[i + 1])) {
+					arr[i + 1] = arr[i + 1].toUpperCase();
+				}
+			}
+		}
+
+		for (int i = 1; i < s.length(); i++) {
+			answer = answer.append(arr[i]);
+		}
+
+		String answer1 = answer.toString();
+		return answer1;
+	}
+}
