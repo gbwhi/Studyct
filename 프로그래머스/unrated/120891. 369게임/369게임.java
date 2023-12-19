e9dbc983f5eaa06da09a2a531c74d12b4c488463
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-12-20 00:45:22 +0900

    [level unrated] Title: 369게임, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120891. 369게임/369게임.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order>0){
+            if(order%10==3){
+                answer++;
+                order=order/10;
+            }else if(order%10==6){
+                answer++;
+                order=order/10;
+            }else if(order%10==9){
+                answer++;
+                order=order/10;
+            }else {
+                order=order/10;
+            }
+        }
+        return answer;
+    }
+}
