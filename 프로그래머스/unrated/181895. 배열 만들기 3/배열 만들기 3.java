17ba7db1a97bc0840a01ec17d81e65ebc90e9198
gbwhi <140616715+gbwhi@users.noreply.github.com>
2023-12-24 23:05:45 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.79 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            
+        
+        for(int i=0;i<intervals.length;i ++){
+            for(int j = intervals[i][0] ; j<= intervals[i][1] ; j++){
+                list.add(arr[j]);
+            }
+        }
+            
+        
+        int[] answer = new int[list.size()];
+    for (int i = 0 ; i < list.size() ; i++) {
+        answer[i] = list.get(i).intValue();
+        }
+        
+        return answer;
+    }
+}
